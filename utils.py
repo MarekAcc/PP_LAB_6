438c1ca599218b1a038d91bfd9cfbdb19f5a1c40
MarekAcc <marekwasiewiczit@gmail.com>
2025-05-07 16:16:59 +0200

    Utils refactoring

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
-
-def add (a: int , b: int) -> int :
+"""Simple calculator"""
+def add(a: int, b: int) -> int:
+    """Add function"""
     return a + b
 
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
+    """Substract function"""
     return a - b
-def multiply (a: int , b: int) -> int :
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply function"""
     return a * b
-def divide ( a: int , b : int ) -> float :
+
+
+def divide(a: int, b: int) -> float:
+    """Divide function"""
     return a / b
